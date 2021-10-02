8385e1bf4a1872d81fa8c3fd7abd4ea2a0ff7c3e
Mohd Asif Momin <40919247+computervisionpro@users.noreply.github.com>
2021-10-02 21:33:39 +0530

    Add code

---
+++ clahe/tutorial15.py
@@ -0,0 +1,14 @@
+import ffmpeg
+import sys
+
+sys.path.append(r'C:\Program Files\ffmpeg-4.3\bin')
+
+stream = ffmpeg.input('sea-video.mp4')
+
+stream = stream.trim(start = 10, duration=15).filter('setpts', 'PTS-STARTPTS')
+stream = stream.filter('fps', fps=5, round='up').filter('scale', w=128, h=128)
+
+stream = ffmpeg.output(stream, 'output.mp4')
+
+ffmpeg.run(stream)
+
