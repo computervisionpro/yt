f6927ba1a16fcb450598952c3c2f150b6f2450c5
Mohd Asif Momin <40919247+computervisionpro@users.noreply.github.com>
2021-10-06 13:26:11 +0530

    Add code

---
+++ counting/tutorial3.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread('coins.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray, cmap='gray');
+
+blur = cv2.GaussianBlur(gray, (11,11), 0)
+plt.imshow(blur, cmap='gray')
+
+canny = cv2.Canny(blur, 30, 150, 3)
+plt.imshow(canny, cmap='gray')
+
+dilated = cv2.dilate(canny, (1,1), iterations = 2)
+plt.imshow(dilated, cmap='gray')
+
+(cnt, heirarchy) = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+cv2.drawContours(rgb, cnt, -1, (0,255,0), 2)
+
+plt.imshow(rgb)
+
+
+print('Coins in the image: ', len(cnt))
